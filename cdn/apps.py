eca69c3d6e3e9a9d7f531a97108d7fa9208432e3
Armin Fekri <armiin.fekri1@gmail.com>
2025-01-19 02:05:57 +0330

    updated apps.py

--- cdn/apps.py
+++ cdn/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class CdnConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'StrategyUpdateDj.cdn_package.cdn'
+    name = 'cdn'
