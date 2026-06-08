02721e7b8dc9b170adbc571a7776226dfb619749
Armin Fekri <armiin.fekri1@gmail.com>
2026-06-08 14:26:46 +0330

    merged with stream, versioned

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="cdn_package",
-    version="1.1.1",
+    version="1.2.0",
     packages=find_packages(),
     install_requires=[
         "grpcio==1.78.0",
